bc4cb3a42bb2a54cda7b838a28036a9981f9e109
Robert Diers <robertdiers@gmail.com>
2026-06-16 20:38:49 +0200

    autostart fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ if [ -z "$WALLET_ADDRESS" ]; then
     exit 1
 fi
 
+echo "Running randomx_boost..."
+./randomx_boost.sh
+
 # install miner if not already installed
 if [ ! -f "$HOME/moneroocean/miner.sh" ]; then
     echo "Installing moneroocean miner..."
@@ -18,9 +21,6 @@ sed -i 's|/root/moneroocean/miner.sh|#/root/moneroocean/miner.sh|g' "$HOME/.prof
 
 echo "Setting max threads hint to $MAX_THREADS_HINT..."
 sed -i 's|"max-threads-hint": [0-9]*,|"max-threads-hint": '"$MAX_THREADS_HINT"',|g' "$HOME/moneroocean/config.json"
-
-echo "Running randomx_boost..."
-./randomx_boost.sh
 
 echo "Starting cron for metrics..."
 cron -f
