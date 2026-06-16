e150ad0ef13e7da139835f816990163bbc506b37
Robert Diers <robertdiers@gmail.com>
2026-06-16 20:39:54 +0200

    autostart fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,6 @@ if [ ! -f "$HOME/moneroocean/miner.sh" ]; then
     curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s "$WALLET_ADDRESS"
 fi
 
-echo "Deactivating miner auto-start in .profile..."
-sed -i 's|/root/moneroocean/miner.sh|#/root/moneroocean/miner.sh|g' "$HOME/.profile"
-
 echo "Setting max threads hint to $MAX_THREADS_HINT..."
 sed -i 's|"max-threads-hint": [0-9]*,|"max-threads-hint": '"$MAX_THREADS_HINT"',|g' "$HOME/moneroocean/config.json"
 
